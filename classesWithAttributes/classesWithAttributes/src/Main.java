5e089ce80997ef64ea45780121ad69573fdf5fce
Ersin ÇETİN <ersin.cetin@hotmail.com>
2022-09-23 22:36:58 +0300

    Update

--- classesWithAttributes/classesWithAttributes/src/Main.java
+++ classesWithAttributes/classesWithAttributes/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product = new Product();
+        product.name = "Laptop";
+        product.id = 1;
+        product.description = "Asus Laptop";
+        product.price = 5000;
+        product.stockAmount = 5;
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+
+        //System.out.println(product.name);
     }
 }
--- classesWithAttributes/classesWithAttributes/src/Product.java
+++ classesWithAttributes/classesWithAttributes/src/Product.java
@@ -1,3 +1,7 @@
 public class Product {
-    
+    int id;
+    String name;
+    String description;
+    double price;
+    int stockAmount;
 }
