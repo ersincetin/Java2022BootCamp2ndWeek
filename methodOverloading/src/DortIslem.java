36fb57a5b511efcfbc34a70ae78c6c7b8f9c7cde
Ersin ÇETİN <ersin.cetin@hotmail.com>
2022-09-24 09:19:02 +0300

    Update

---
+++ methodOverloading/src/DortIslem.java
@@ -0,0 +1,12 @@
+public class DortIslem {
+
+    public int topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    public int topla(int sayi1, int sayi2, int sayi3) {
+        return sayi1 + sayi2 + sayi3;
+    }
+
+    /*Aynı isimdeki iki farklı methodu farklı parametreler ile overloading yapmış olduk.*/
+}
