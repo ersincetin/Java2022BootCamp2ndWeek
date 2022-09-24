24b4d2d315dae10ed3691dc217c2c56a82fc2d7b
Ersin ÇETİN <ersin.cetin@hotmail.com>
2022-09-24 09:37:19 +0300

    Update

--- inheritance/src/Main.java
+++ inheritance/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer customer = new Customer();
+        Employee employee = new Employee();
+
+        EmployeeManager employeeManager = new EmployeeManager();
+        CustomerManager customerManager = new CustomerManager();
     }
 }
