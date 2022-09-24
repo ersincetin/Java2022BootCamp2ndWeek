62fd4d4e8499a84a2dccd6e597156e96950a0404
Ersin ÇETİN <ersin.cetin@hotmail.com>
2022-09-24 10:15:04 +0300

    Update

--- inheritanceDemo/src/Main.java
+++ inheritanceDemo/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
+//        ogretmenKrediManager.Hesapla();
+
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new OgretmenKrediManager());
+        krediUI.KrediHesapla(new TarimKrediManager());
+        krediUI.KrediHesapla(new AskerKrediManager());
     }
 }
