b17fd5edcbe498c98ef7de158e344fb55ba34fd1
Ersin ÇETİN <ersin.cetin@hotmail.com>
2022-09-24 09:05:39 +0300

    Update

--- getterSetter/src/Main.java
+++ getterSetter/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product();
-        product.setName("Laptop");
-        product.setId(1);
-        product.setDescription("Asus Laptop");
-        product.setPrice(5000);
-        product.setStockAmount(5);
+
+        // Bu kısımda kullandığımızda parametresiz Constructor çalışmış olacak.
+//        Product product = new Product();
+//        product.setName("Laptop");
+//        product.setId(1);
+//        product.setDescription("Asus Laptop");
+//        product.setPrice(5000);
+//        product.setStockAmount(5);
+
+        Product product = new Product(1, "Laptop", "Asus Laptop", 5000, 5);
 
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
--- getterSetter/src/Product.java
+++ getterSetter/src/Product.java
@@ -6,6 +6,17 @@ public class Product {
     private int stockAmount;
     private String kod;
 
+    public Product() {
+    }
+
+    public Product(int id, String name, String description, double price, int stockAmount) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockAmount = stockAmount;
+    }
+
     public int getId() {
         return id;
     }
